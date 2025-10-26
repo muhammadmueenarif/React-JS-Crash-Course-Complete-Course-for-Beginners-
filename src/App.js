c4ceef1e0b37c16a94e681fb49c4331b73c3f8b7
muhammadmueenarif <muhammadmueenarif786>
2025-10-26 23:15:48 +0500

    material ui

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useState, useLocation, useEffect } from "react";
 import "./App.css";
 import Pokemon from "./components/Pokemon";
 import EmailIcon from '@mui/icons-material/Email';
+import Home from "./components/Home";
+import Counter from "./components/counter";
+import Email from "@mui/icons-material/Email";
 
 function App() {
   return (
     <>
-    <EmailIcon/>
+   <Email/>
     </>
   );
 }
